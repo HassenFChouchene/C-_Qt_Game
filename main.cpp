49a62433edc1b188b5758437de6c1feb455571c9
Hassen <hassenchouchene99@gmail.com>
2023-04-28 22:00:33 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //Create a scene
+
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    QGraphicsRectItem * rect = new  QGraphicsRectItem();
+
+    rect->setRect(0,0,100,100);
+
+    scene->addItem(rect);
+
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
+
+
+
+    return a.exec();
+}
